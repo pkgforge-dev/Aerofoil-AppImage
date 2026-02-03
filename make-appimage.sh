dec043819f04273e6329a58f7051b386ec46d464
Link4Electronics <link4electronics@gmail.com>
2026-02-03 02:26:43 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,8 +14,8 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/AerofoilX
-cp -r /usr/lib/aerofoil/Packaged ./AppDir/shared/lib
-cp -r /usr/lib/aerofoil/tools ./AppDir/shared/lib
+cp -r /usr/lib/aerofoil/Packaged ./AppDir/bin
+cp -r /usr/lib/aerofoil/tools ./AppDir/bin
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
