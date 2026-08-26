529dc47bc45038a2ba5783595f6304df3581db59
Link4Electronics <link4electronics@gmail.com>
2026-08-26 05:15:23 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,25 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#pacman -Syu --noconfirm
+pacman -Syu --noconfirm cmake
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano libdecor-mini
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini sdl2_image-mini
 
 # Comment this out if you need an AUR package
 make-aur-package aerofoil-git
 
 # If the application needs to be manually built that has to be done down here
+
+
+echo "Building Aerofoil..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/elasota/Aerofoil"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./Aerofoil
+echo "$VERSION" > ~/version
+
+cd ./Aerofoil
+cmake -DCMAKE_BUILD_TYPE=Release -B build
+cmake --build build
