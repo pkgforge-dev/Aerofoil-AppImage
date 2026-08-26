67cc89f99e1b9293cacfaae86ca82716ccfe6fe1
Link4Electronics <link4electronics@gmail.com>
2026-08-26 05:24:31 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,13 +7,14 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.elasota.aerofoil.svg
-export DESKTOP=/usr/share/applications/io.github.elasota.aerofoil.desktop
+export ICON=https://raw.githubusercontent.com/elasota/Aerofoil/eccf2cf528af04cd87d741719db34d7745c05be5/Resources/Linux/io.github.elasota.aerofoil.svg
+export DESKTOP=https://raw.githubusercontent.com/elasota/Aerofoil/refs/heads/master/Resources/Linux/io.github.elasota.aerofoil.desktop
 export STARTUPWMCLASS=AerofoilX
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/AerofoilX /usr/lib/aerofoil
+#quick-sharun /usr/bin/AerofoilX /usr/lib/aerofoil
+quick-sharun ./AppDir/bin/*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
