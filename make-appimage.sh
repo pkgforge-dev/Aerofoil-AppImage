c81ec643573412021c48f96eb2630978864344a9
Link4Electronics <link4electronics@gmail.com>
2026-08-29 10:30:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://raw.githubusercontent.com/elasota/Aerofoil/eccf2cf528af04cd87d741719db34d7745c05be5/Resources/Linux/io.github.elasota.aerofoil.svg
 export DESKTOP=https://raw.githubusercontent.com/elasota/Aerofoil/refs/heads/master/Resources/Linux/io.github.elasota.aerofoil.desktop
 export STARTUPWMCLASS=AerofoilX
-export DEPLOY_OPENGL=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/AerofoilX /usr/lib/aerofoil
